ea0a7e48a344ccc5cf1af48c1febc3490fd49414
Satvik <satvik.beura@gmail.com>
2026-04-28 16:45:51 +0530

    Add npm script and modify app meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
   ssr: true,
   app: {
     head: {
-      title: "Word Chain Game",
+      title: "Word Games - Play and Learn",
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
